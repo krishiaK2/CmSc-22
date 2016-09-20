d39e1a9d3b6e0cfb1901bb06b4e9ff19ffbef463
krishiaK2 <2911.samson@gmail.com>
2016-09-20 15:43:58 +0800

    Update Date.java

--- Date.java
+++ Date.java
@@ -7,9 +7,9 @@ import java.lang.*;
 
 public class Date {
 
-	private int year = 1000;
-	private int month = 1;
-	private int day = 1;
+	private int year;
+	private int month;
+	private int day;
 
 	public int getYear() {
 		return year;
@@ -39,29 +39,29 @@ public class Date {
 
 	public void setDay(int day) {
 
-		if (month == 2) {
+		if ((this.month == 2) && !((this.year % 400 == 0) || (this.year % 100 != 0 && this.year % 4 == 0))) {
 			if (day < 1 || day > 28) {
 				throw new IllegalArgumentException("FEB has only 28 days!");
-			} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+			} else if (this.month == 4 || this.month == 6 || this.month == 9 || this.month == 11) {
 				if (day < 1 || day > 30) {
 					throw new IllegalArgumentException("This month has 30 days only!");
 				}
-			} else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			} else if (this.month == 1 || this.month == 3 || this.month == 5 || this.month == 7 || this.month == 8 || this.month == 10 || this.month == 12) {
 				if (day < 1 || day > 31) {
 					throw new IllegalArgumentException("This month has 31 days only!");
 				}
 			}
 		} else {
-			if ((month % 4 == 0) || (month % 100 == 0 && month % 400 == 0)) {
-				if (month == 2) {
+			if ((this.year % 400 == 0) || (this.year % 100 != 0 && this.year % 4 == 0)) {
+				if (this.month == 2) {
 					if (day < 1 || day > 29) {
 						throw new IllegalArgumentException("FEB leap year has 29 days only!");
 					}
-				} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+				} else if (this.month == 4 || this.month == 6 || this.month == 9 || this.month == 11) {
 					if (day < 1 || day > 30) {
 						throw new IllegalArgumentException("This month has 30 days only!");
 					}
-				} else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+				} else if (this.month == 1 || this.month == 3 || this.month == 5 || this.month == 7 || this.month == 8 || this.month == 10 || this.month == 12) {
 					if (day < 1 || day > 31) {
 						throw new IllegalArgumentException("This month has 31 days only!");
 					}
@@ -69,33 +69,28 @@ public class Date {
 			}
 		}
 		this.day = day;
+
 	}
 
-	// public Date() {
+	public Date() {
+		this.year = 1000;
+		this.month = 1;
+		this.day = 1;
+	}
 
-	// 	this.year = 1000;
-	// 	this.month = 1;
-	// 	this.day = 1;
-
-	// }
-
-	public Date(int year, int month, int day) {
-		
+	public Date(int year, int month, int day) {		
 		this.year = year;
 		this.month = month;
 		this.day = day;
-		
 	}
 
 	public void setDate(int year, int month, int day) {
-
-		this.year = year;
-		this.month = month;
-		this.day = day;
-
+		setYear(year);
+		setMonth(month);
+		setDay(day);
 	}
 
 	public String toString() {
-		return String.format("%02d/%02d/%02d", this.day, this.month, this.year);
+		return String.format("%02d/%02d/%02d", day, month, year);
 	}
 }
