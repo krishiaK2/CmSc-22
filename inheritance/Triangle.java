fbac39ee635eefe26e8a5672574f28896f035d62
krishiaK2 <2911.samson@gmail.com>
2016-10-07 16:51:03 +0800

    Update Triangle.java

--- inheritance/Triangle.java
+++ inheritance/Triangle.java
@@ -5,7 +5,7 @@ public class Triangle extends Shape{
 	private double s3;
 
 	public Triangle(double s1, double s2, double s3) {
-		if ((s1 + s2 < s3) || (s1 + s3 < s2) || (s2 + s3 < s1)) {
+		if ((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)) {
 			throw new IllegalArgumentException("illegal sides of a triangle!");
 		} else {
 			this.s1 = s1;
@@ -14,11 +14,54 @@ public class Triangle extends Shape{
 		}
 	}
 
+	public Triangle(double s1, double s2, double s3, String color, boolean filled) {
+		super(color, filled);
+		if ((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)) {
+			throw new IllegalArgumentException("illegal sides of a triangle!");
+		} else {
+			this.s1 = s1;
+			this.s2 = s2;
+			this.s3 = s3;
+			
+		}
+	}
+
+	public double getSide1() {
+		return s1;
+	}
+
+	public double getSide2() {
+		return s2;
+	}
+
+	public double getSide3() {
+		return s3;
+	}
+
+	public void setSide1(double s1) {
+		this.s1 = s1;
+	}
+
+	public void setSide2(double s2) {
+		this.s2 = s2;
+	}
+
+	public void setSide3(double s3) {
+		this.s3 = s3;
+	}
+
+	@Override
 	public double getPerimeter() {
 		return (s1 + s2 + s3);
 	}
 
-	public double getArea() { //sir, dili mi kaimplement sa getArea kay sides ramay parameters, dili base and height.kani amo gihimo kay char char lang para dili muerror kay abstract man gud ang getArea sa superclass. 
-		return 123.22;
+	@Override
+	public double getArea() { 
+		double p = getPerimeter() / 2;
+		return Math.sqrt(p);
+	}
+
+	public String toString() {
+		return String.format("A Triangle with side1 = %f, side2 = %f, and side3 = %f which is a subclass of %s", s1, s2, s3, super.toString());
 	}
 }
