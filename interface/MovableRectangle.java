e5e8674267514c37e70b9ea00f0834895d349bbd
krishiaK2 <2911.samson@gmail.com>
2016-10-12 16:58:04 +0800

    Update MovableRectangle.java

--- interface/MovableRectangle.java
+++ interface/MovableRectangle.java
@@ -1,4 +1,4 @@
-public class MovableRectangle implements Movable {
+public class MovableRectangle implements Movable, Drawable {
 
 	private MovablePoint topLeft;
 	private MovablePoint bottomRight;
@@ -32,8 +32,42 @@ public class MovableRectangle implements Movable {
 		bottomRight.moveRight();
 	}
 
+	// Bonus
+	public void render() {
+		int xLength = Math.abs(topLeft.x - bottomRight.x);
+		int yLength = Math.abs(topLeft.y - bottomRight.y);
+
+		for (int i = 0; i < topLeft.y; i++) {
+			System.out.print("\n");
+		}
+
+			for (int k = 0; k < topLeft.x; k++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < xLength; k++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+			for (int k = 0; k < yLength - 2; k++) {
+				for (int i = 0; i < topLeft.x; i++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+				for (int j = 0; j < xLength - 2; j++) {
+					System.out.print(" ");
+				}
+				System.out.println("*");
+			}
+			for (int k = 0; k < topLeft.x; k++) {
+				System.out.print(" ");
+			}
+			for (int z = 0; z < xLength; z++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+	}
+
 	public String toString() {
 		return String.format("x1 = %d, y1 = %d, x2 = %d, y2 = %d, x1Speed = %d, y1Speed = %d, x2Speed = %d, y2Speed = %d", topLeft.x, topLeft.y, bottomRight.x, bottomRight.y, topLeft.xSpeed, topLeft.ySpeed, bottomRight.xSpeed, bottomRight.ySpeed);
 	}
-
 }
