3d6868690b84992c86547466e8759fcb6ad1d326
krishiaK2 <2911.samson@gmail.com>
2016-10-12 16:58:42 +0800

    Update MovableCircle.java

--- interface/MovableCircle.java
+++ interface/MovableCircle.java
@@ -28,6 +28,38 @@
 		center.moveRight(); //center.x += center.xSpeed; 
 	}
 
+	public void render() {
+		for (int i = 0; i < (center.y - radius); i++) {
+			System.out.println();
+		}
+		for (int i = 0; i < center.x; i++) {
+			System.out.print(".");
+		}
+		System.out.println("*"); // up
+		for (int i = 0; i < radius; i++) {
+			System.out.println();
+		}
+		for (int i = 0; i < (center.x - radius); i++) {
+			System.out.print(".");	
+		}
+		System.out.print("*"); // left
+		for (int i = 0; i < (radius - 1); i++) {
+			System.out.print(".");	
+		}
+		System.out.print("*"); // center
+		for (int i = 0; i < (radius - 1); i++) {
+			System.out.print(".");
+		}
+		System.out.println("*"); // right
+		for (int i = 0; i < (radius - 1); i++) {
+			System.out.println();
+		}
+		for (int i = 0; i < center.x; i++) {
+			System.out.print(".");
+		}
+		System.out.println("*"); // down
+	}
+
 	public String toString() {
 		return String.format("[radius = %d, center.x = %d, center.y = %d, xSpeed = %d, ySpeed = %d", radius, center.x, center.y, center.xSpeed, center.ySpeed);
 	}
